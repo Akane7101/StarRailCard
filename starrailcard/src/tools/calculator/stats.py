f05d45b2d81bb7bf23891cf486edbf2e75cc9aa8
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2024-12-17 00:49:03 +0200

    Update stats.py
    Signed-off-by: Akane7101 <129458531+Akane7101@users.noreply.github.com>

--- starrailcard/src/tools/calculator/stats.py
+++ starrailcard/src/tools/calculator/stats.py
@@ -103,7 +103,7 @@ class Calculator:
 
         return result_json,dont_sub
 
-    async def start(self, hoyo):
+   async def start(self, hoyo):
     if not self.data.id in self.score:
         await self.update_score(self.data.id)
         self.score = open_score("score")
